2895a4ba92ff390bf2dd406ae9dfcb2c1fc206ab
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-08 10:19:43 -0500

    has_negatives complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,10 +1,18 @@
 def has_negatives(a):
 
-    """
-    YOUR CODE HERE
-    """
+    #return empty list if no negatives found
+    #if negatives found, return list with those numbers (no repeats)
+    #only return positive numbers that have a corresponding negative
 
-    return result
+    #make hash table with k(number):v(0) if number > 0
+    result = {k:0 for k in a if k > 0}
+    for i in a:
+        #if the number is negative, and there is an occurrance of the absolute value in our hashtable, make it True
+        if i < 0 and result[abs(i)] == 0:
+            result[abs(i)] = True
+        
+    #reutrn list of True values from the ht
+    return [x[0] for x in result.items() if x[1]]
 
 
 if __name__ == "__main__":
