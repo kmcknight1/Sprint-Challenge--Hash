f0ba58f5c52c35f8454cb36a95e8fea8dc0b76de
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-08 10:56:45 -0500

    attempted, not complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,25 @@
 def intersection(arrays):
 
-    """
-    YOUR CODE HERE
-    """
+    #return numbers that appear in all lists 
+    #if number is same as number of lists, true
 
-    return result
+    length = len(arrays)
+
+    ht = {k:1 for k in arrays[0]}
+
+
+    arrays = [x for x in arrays if arrays.index(x) != 0]
+
+
+    for i in arrays:
+        for j in ht:
+            if j in i:
+                ht[j] += 1
+
+    print(ht)
+    print(length)
+    print([x for x in ht if ht[x] == length])
+    return [x for x in ht if ht[x] == length]
 
 
 if __name__ == "__main__":
