c772503ae9bba82013f5854c4f30f4e42ca6d703
kmcknight1 <katiemcknight1991@gmail.com>
2020-05-08 10:54:53 -0500

    reconstruct_trip complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
+    ht = {}
+    route = [None] * length
 
+    #add tickets to ht
+    for t in tickets:
+        ht[t.source] = t.destination
+
+    #start at current city where source is None
+    current_city = ht["NONE"]
+    #loop through, starting at current_city
+    #add the destinations to the route array
+    #next city is the ht[current_city]
+    i = 0
+    while current_city != "NONE":
+        route[i] = current_city
+        current_city = ht[current_city]
+        i += 1
+    #make the last one None
+    route[i] = "NONE"
+    
     return route
